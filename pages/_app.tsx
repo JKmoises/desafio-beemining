1465ae8db35dd21639877ac83782e050447877e9
JKmoises <moi.prado@duocuc.cl>
2023-02-15 21:41:45 -0300

    Preparando proyecto para deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app'
-import '@/styles/globals.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }: AppProps) {
